52a82ddb81a1dca18c0d4d873b23d3bd9926a670
rlorona <rlorona@espol.edu.ec>
2025-01-15 13:36:09 -0500

    Dependencias agregadas a los gradle

--- App AndroidStudio/app/build.gradle.kts
+++ App AndroidStudio/app/build.gradle.kts
@@ -1,4 +1,7 @@
 plugins {
+    id("com.android.application")
+    // Add the Google services Gradle plugin
+    id("com.google.gms.google-services")
     alias(libs.plugins.android.application)
 }
 
@@ -32,7 +35,17 @@ android {
 }
 
 dependencies {
+    // Import the Firebase BoM
+    implementation(platform("com.google.firebase:firebase-bom:33.7.0"))
 
+
+    // TODO: Add the dependencies for Firebase products you want to use
+    // When using the BoM, don't specify versions in Firebase dependencies
+    implementation("com.google.firebase:firebase-analytics")
+
+
+    // Add the dependencies for any other desired Firebase products
+    // https://firebase.google.com/docs/android/setup#available-libraries
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
--- App AndroidStudio/build.gradle.kts
+++ App AndroidStudio/build.gradle.kts
@@ -1,4 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
+    //Agregar dependencias a Google services gradle plugin
+    id("com.google.gms.google-services") version "4.4.2" apply false
     alias(libs.plugins.android.application) apply false
 }
