4caddf2c823912ebb1290068aef142648df4ced9
Manuel <maanjime@espol.edu.ec>
2025-01-15 05:47:56 -0500

    Interfaces principales

--- App AndroidStudio/app/src/main/java/com/example/pstg1/MainActivity.java
+++ App AndroidStudio/app/src/main/java/com/example/pstg1/MainActivity.java
@@ -1,7 +1,8 @@
 package com.example.pstg1;
 
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.widget.Button;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -9,16 +10,35 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private Button BotonTemp, BotonConsumo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        // Inicializar los botones
+        BotonTemp = findViewById(R.id.BotonTemp);
+        BotonConsumo = findViewById(R.id.BotonConsumo);
+
+        // Ajustar el padding de las vistas
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        // Listener para BotonTemp -> TempyhumedadActivity
+        BotonTemp.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, TempyhumedadActivity.class);
+            startActivity(intent);
+        });
+
+        // Listener para BotonConsumo -> ConsumoElecActivity
+        BotonConsumo.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, ConsumoElecActivity.class);
+            startActivity(intent);
         });
     }
 }
---
+++ App AndroidStudio/app/src/main/java/com/example/pstg1/TempyhumedadActivity.java
@@ -0,0 +1,12 @@
+package com.example.pstg1;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TempyhumedadActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tempyhumedad);
+    }
+}
